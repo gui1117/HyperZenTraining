93875a75fe9d61076b25a9fad6dd0a6249a8acc5
thiolliere <gui.thiolliere@gmail.com>
2018-01-08 20:57:43 +0100

    some coef

--- src/system/draw.rs
+++ src/system/draw.rs
@@ -84,7 +84,7 @@ impl<'a> ::specs::System<'a> for DrawSystem {
                 ::std::f32::consts::FRAC_PI_3,
                 // IDEA: change to 0.0001 it's funny
                 0.05 * depth_coef.0,
-                70.0,
+                150.0,
             ).unwrap();
 
             let view_uniform = ::graphics::shader::draw1_vs::ty::View {
